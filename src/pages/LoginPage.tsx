c8892227b777ce6df0283cb73b3de961330b3bb1
Adrian Nads <marcelonads@gmail.com>
2023-08-23 05:32:43 +0800

    ADDED login functionality to LoginPage

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,26 +1,69 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
 
 function LoginPage() {
+
+    const navigate = useNavigate();
+    const [isLoading, setLoading] = useState<boolean>(() => false);
+    const [username, setUsername] = useState<string>(() => '');
+    const [password, setPassword] = useState<string>(() => '');
+    const [isError, setError] = useState<boolean>(() => false);
+
+    const login = async (e) => {
+
+        e.preventDefault();
+
+        setLoading(true);
+
+        try {
+
+            const response = await axios.post('http://localhost:3001/account/login', { username, password }, { withCredentials: true });
+
+            if (response.status >= 200 && response.status <= 206) {
+
+                setLoading(false);
+                navigate('/home');
+            }
+        }
+        catch (err) {
+            setLoading(false);
+            setError(true);
+
+            setTimeout(() => {
+
+                setError(false);
+            }, 2000);
+        }
+    }
     return (
         <main className='login-page container-fluid row justify-content-center align-items-center'>
 
             <div className='form-wrapper'>
 
                 <h5>Login</h5>
-                <form action="" className='d-flex flex-column'>
+                <form onSubmit={login} className='d-flex flex-column'>
 
                     <div>
                         <label htmlFor="username" className='form-label'>Username</label>
-                        <input className='form-control' type="text" name="username" id="username" />
+                        <input onChange={(e) => setUsername(e.currentTarget.value)} className='form-control' type="text" name="username" id="username" />
                     </div>
 
                     <div className='mt-3'>
                         <label htmlFor="password" className='form-label'>Password</label>
-                        <input className='form-control' type="text" name="password" id="password" />
+                        <input onChange={(e) => setPassword(e.currentTarget.value)} className='form-control' type="password" name="password" id="password" />
                     </div>
 
-                    <button className="btn btn-primary mt-3 align-self-end">Login</button>
+                    <button className="btn btn-primary mt-3 align-self-end d-flex align-items-center gap-3">
+                        <span>{isLoading ? 'Please Wait' : 'Login'}</span>
+                        {isLoading && <div className="spinner-grow spinner-grow-sm text-info" role="status">
+                            <span className="visually-hidden">Login</span>
+                        </div>}
+                    </button>
                 </form>
+                {isError && <div className="alert alert-danger mt-3" role="alert">
+                    Invalid credentials. Please check your username and password.
+                </div>}
             </div>
         </main>
     )
