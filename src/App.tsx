56a1a56cf93c849a323c4facb11fd432d81055b1
Adrian Nads <marcelonads@gmail.com>
2023-08-23 03:55:52 +0800

    done for setting up the login page design

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import { createBrowserRouter, Route, createRoutesFromElements, RouterProvider } from 'react-router-dom';
+import LoginPage from './pages/LoginPage';
+import Home from './pages/Home';
+import './App.css';
 
 const router = createBrowserRouter(
 
   createRoutesFromElements(
     <Route>
-
+      <Route path="/account/login" element={<LoginPage />} />
+      <Route path="/home/" element={<Home />} />
     </Route>
   )
 )
--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -2,7 +2,27 @@ import React from 'react'
 
 function LoginPage() {
     return (
-        <div>LoginPage</div>
+        <main className='login-page container-fluid row justify-content-center align-items-center'>
+
+            <div className='form-wrapper'>
+
+                <h5>Login</h5>
+                <form action="" className='d-flex flex-column'>
+
+                    <div>
+                        <label htmlFor="username" className='form-label'>Username</label>
+                        <input className='form-control' type="text" name="username" id="username" />
+                    </div>
+
+                    <div className='mt-3'>
+                        <label htmlFor="password" className='form-label'>Password</label>
+                        <input className='form-control' type="text" name="password" id="password" />
+                    </div>
+
+                    <button className="btn btn-primary mt-3 align-self-end">Login</button>
+                </form>
+            </div>
+        </main>
     )
 }
 
