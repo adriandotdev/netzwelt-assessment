6afc3bfb92c2c75b7826def5f55651592ed8048c
Adrian Nads <marcelonads@gmail.com>
2023-08-23 06:14:30 +0800

    ADDED HomeVerifierLoader at Home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,20 @@
 import React from 'react'
-import { createBrowserRouter, Route, createRoutesFromElements, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, Route, createRoutesFromElements, RouterProvider, Navigate } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
 import Home from './pages/Home';
 import './App.css';
 
 // Loaders
 import { VerifierLoader } from './pages/LoginPage';
+import { HomeVerifierLoader } from './pages/Home';
 
 const router = createBrowserRouter(
 
   createRoutesFromElements(
     <Route>
+      <Route path="/" element={<Navigate to="/home" />} />
       <Route path="/account/login" element={<LoginPage />} loader={VerifierLoader} />
-      <Route path="/home/" element={<Home />} />
+      <Route index path="/home" element={<Home />} loader={HomeVerifierLoader} />
     </Route>
   )
 )
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,175 +1,28 @@
 import Location from '../components/Location';
-import Territory from '../interfaces/Territory';
+import { useNavigate, Navigate, useLoaderData } from 'react-router-dom';
+import axios from 'axios';
 
 function Home() {
 
-    const data: Territory[] = [
-        {
-            "id": "1",
-            "name": "Metro Manila",
-            "parent": null,
-            "children": [
-                {
-                    "id": "101",
-                    "name": "Manila",
-                    "parent": "1",
-                    "children": [
-                        {
-                            "id": "10101",
-                            "name": "Malate",
-                            "parent": "101"
-                        },
-                        {
-                            "id": "10102",
-                            "name": "Ermita",
-                            "parent": "101"
-                        },
-                        {
-                            "id": "10103",
-                            "name": "Binondo",
-                            "parent": "101"
-                        }
-                    ]
-                },
-                {
-                    "id": "102",
-                    "name": "Makati",
-                    "parent": "1",
-                    "children": [
-                        {
-                            "id": "10201",
-                            "name": "Poblacion",
-                            "parent": "102"
-                        },
-                        {
-                            "id": "10202",
-                            "name": "Bel-Air",
-                            "parent": "102"
-                        },
-                        {
-                            "id": "10203",
-                            "name": "San Lorenzo",
-                            "parent": "102"
-                        },
-                        {
-                            "id": "10204",
-                            "name": "Urdaneta",
-                            "parent": "102"
-                        }
-                    ]
-                },
-                {
-                    "id": "103",
-                    "name": "Marikina",
-                    "parent": "1",
-                    "children": [
-                        {
-                            "id": "10301",
-                            "name": "Sto Nino",
-                            "parent": "103"
-                        },
-                        {
-                            "id": "10302",
-                            "name": "Malanday",
-                            "parent": "103"
-                        },
-                        {
-                            "id": "10303",
-                            "name": "Concepcion I",
-                            "parent": "103"
-                        }
-                    ]
-                }
-            ]
-        },
-        {
-            "id": "2",
-            "name": "CALABARZON",
-            "parent": null,
-            "children": [
-                {
-                    "id": "201",
-                    "name": "Laguna",
-                    "parent": "2",
-                    "children": [
-                        {
-                            "id": "20101",
-                            "name": "Calamba",
-                            "parent": "201"
-                        },
-                        {
-                            "id": "20102",
-                            "name": "Sta. Rosa",
-                            "parent": "201"
-                        }
-                    ]
-                },
-                {
-                    "id": "202",
-                    "name": "Cavite",
-                    "parent": "2",
-                    "children": [
-                        {
-                            "id": "20201",
-                            "name": "Kawit",
-                            "parent": "202"
-                        }
-                    ]
-                },
-                {
-                    "id": "203",
-                    "name": "Batangas",
-                    "parent": "2",
-                    "children": [
-                        {
-                            "id": "20301",
-                            "name": "Lipa",
-                            "parent": "203"
-                        },
-                        {
-                            "id": "20302",
-                            "name": "Tanauan",
-                            "parent": "203"
-                        }
-                    ]
-                }
-            ]
-        },
-        {
-            "id": "3",
-            "name": "Central Luzon",
-            "parent": null,
-            "children": [
-                {
-                    "id": "301",
-                    "name": "Bulacan",
-                    "parent": "3"
-                },
-                {
-                    "id": "302",
-                    "name": "Nueva Ecija",
-                    "parent": "3"
-                },
-                {
-                    "id": "303",
-                    "name": "Tarlac",
-                    "parent": "3"
-                },
-                {
-                    "id": "304",
-                    "name": "Pampanga",
-                    "parent": "3"
-                }
-            ]
-        }
-    ]
+    const navigate = useNavigate();
+    const data = useLoaderData();
+
+    const logout = async () => {
+
+        await axios.post('http://localhost:3001/account/logout', {}, { withCredentials: true });
+        navigate('/account/login');
+    }
+
+    if (!data) {
+        return <Navigate to="/account/login" replace={true} />
+    }
 
     return (
         <main className='mx-5 my-4'>
 
             <div className='mx-3 d-flex align-items-center gap-3'>
                 <h1 >Territories</h1>
-                <button className='btn btn-danger'>Logout</button>
+                <button onClick={logout} className='btn btn-danger'>Logout</button>
             </div>
 
             <p className='mx-3'>Here are the list of territories</p>
@@ -180,6 +33,20 @@ function Home() {
             </ul>
         </main>
     )
+}
+
+export const HomeVerifierLoader = async () => {
+
+    try {
+
+        const response = await axios.get('http://localhost:3001/home/index', { withCredentials: true });
+
+        return response.data;
+    }
+    catch (err) {
+
+        return null;
+    }
 }
 
 export default Home
