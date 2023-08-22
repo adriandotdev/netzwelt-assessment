67864a1278e2bd6e3eb50db59ccf9df6dce8b13b
Adrian Nads <marcelonads@gmail.com>
2023-08-23 04:45:34 +0800

    created Location component
    this component is responsible for rendering the territories

--- src/components/Location.tsx
+++ src/components/Location.tsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import { useRef } from 'react'
 
 function Location({ location }) {
+
+    const territories = useRef(null);
+
     return (
-        <div>Location</div>
+        <li key={location.id}>
+            <span onClick={(e) => {
+
+                territories.current.classList.toggle('active');
+                e.currentTarget.classList.toggle('caret-down');
+            }} className='caret'>{location.name}</span>
+            {
+                location?.children?.length > 0 && (
+
+                    <ul ref={territories} className='nested'>
+                        {location.children.map(territory => {
+
+                            return (
+                                <Location location={territory} />
+                            )
+                        })}
+                    </ul>
+                )
+            }
+        </li>
     )
 }
 
